0718b77d3ac5bbf4e2a4b2652de10dd714308f9d
gaga5lala <gaga5lala1013@gmail.com>
2016-11-07 00:32:32 +0800

    Add spgateway integration
    * serivce url
    * transation query url

---
+++ lib/offsite_payments/integrations/spgateway.rb
@@ -0,0 +1,33 @@
+module OffsitePayments
+  module Integrations
+    module Spgateway
+      mattr_accessor :test_service_url
+      self.test_service_url = "https://ccore.spgateway.com/MPG/mpg_gateway"
+
+      mattr_accessor :production_service_url
+      self.production_service_url = "https://core.spgateway.com/MPG/mpg_gateway"
+
+      mattr_accessor :test_confirmation_url
+      self.test_confirmation_url = ":https://ccore.spgateway.com/API/QueryTradeInfo"
+
+      mattr_accessor :production_confirmation_url
+      self.production_confirmation_url = "https://core.spgateway.com/API/QueryTradeInfo"
+
+      def self.service_url
+        self.production_service_url
+      end
+
+      def self.confirmation_url
+        self.production_confirmation_url
+      end
+
+      def self.setup
+        yield(self)
+      end
+
+      def self.notification(post, options = {})
+        Notification.new(post)
+      end
+    end
+  end
+end
