dff607afd7bed37731d34f6572d207ae178322ac
gaga5lala <gaga5lala1013@gmail.com>
2016-11-09 01:31:17 +0800

    Extend action_view_ls helper

--- lib/active_merchant_spgateway.rb
+++ lib/active_merchant_spgateway.rb
@@ -1,5 +1,8 @@
 require "active_merchant_spgateway/version"
+require "action_view"
 
 module ActiveMerchantSpgateway
   # Your code goes here...
 end
+
+ActionView::Base.send(:include, OffsitePayments::ActionViewHelper)
